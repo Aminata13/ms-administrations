416223af856b16f846efd3a375b005abd21dda38
Ousmane Ndiaye <ousmanendiaye352@gmail.com>
2021-06-08 15:47:12 +0000

    move cors config in gateway

--- src/main/java/com/safelogisitics/gestionentreprisesusers/GestionEntreprisesUsersApplication.java
+++ src/main/java/com/safelogisitics/gestionentreprisesusers/GestionEntreprisesUsersApplication.java
@@ -1,14 +1,8 @@
 package com.safelogisitics.gestionentreprisesusers;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -17,19 +11,5 @@ public class GestionEntreprisesUsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionEntreprisesUsersApplication.class, args);
-	}
-
-  @Bean
-	public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(false);
-
-		// Don't do this in production, use a proper list  of allowed origins
-    config.setAllowedOrigins(Collections.singletonList("*"));
-    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
-    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-    source.registerCorsConfiguration("/**", config);
-    return new CorsFilter(source);
 	}
 }
